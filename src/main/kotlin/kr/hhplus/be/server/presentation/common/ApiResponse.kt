d2487c73c9165bcbf664624838d1b62b60962ed7
LEECHANHEE <chanhee1158@gmail>
2025-04-03 23:38:13 +0900

    <Feat>  #5: API Response의 일관된 구조 설계

---
+++ src/main/kotlin/kr/hhplus/be/server/presentation/common/ApiResponse.kt
@@ -0,0 +1,40 @@
+package kr.hhplus.be.server.presentation.common
+
+import kr.hhplus.be.server.presentation.common.enums.BusinessSuccessCode
+
+data class ApiResponse<T>(
+    val status: Int,
+    val code: String,
+    val message: String,
+    val data: T? = null
+) {
+    companion object {
+
+        //@SuccessAnnotation 적용 시 적용
+        fun <T> success(
+            status: Int = 200,
+            code: String,
+            data: T?,
+            message: String = "요청이 성공적으로 처리되었습니다.",
+        ): ApiResponse<T> =
+            ApiResponse(status = status, code = code, message = message, data = data)
+
+        //@SuccessAnnotation 미적용 시 적용
+        fun <T> success(
+            data: T?,
+        ): ApiResponse<T> =
+            ApiResponse(
+                status = BusinessSuccessCode.SUCCESS.status,
+                code = BusinessSuccessCode.SUCCESS.code,
+                message = BusinessSuccessCode.SUCCESS.message,
+                data = data
+            )
+
+        fun <T> error(
+            status: Int = 400,
+            code: String,
+            message: String = "잘못된 요청입니다.",
+        ): ApiResponse<T> =
+            ApiResponse(status = status, code = code, message = message, data = null)
+    }
+}
---
+++ src/main/kotlin/kr/hhplus/be/server/presentation/common/BusinessCode.kt
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.presentation.common
+
+interface BusinessCode {
+    val code: String
+    val message: String
+    val status: Int
+}
