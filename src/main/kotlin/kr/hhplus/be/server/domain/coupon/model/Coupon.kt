1d5352e1fe0e2d0e084171cc0296334ea4950f48
LEECHANHEE <chanhee1158@gmail>
2025-04-11 00:44:43 +0900

    <Refactor> #9: 쿠폰 할인 가격 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/coupon/model/Coupon.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/coupon/model/Coupon.kt
@@ -4,8 +4,9 @@ import java.time.LocalDateTime
 
 class Coupon(
     val id: Long = 0,
-    var amount: Long,
-    var name: String,
+    val amount: Long,
+    val discountPrice: Long,
+    val name: String,
     var createdAt: LocalDateTime = LocalDateTime.now(),
     var updatedAt: LocalDateTime = LocalDateTime.now(),
 )
