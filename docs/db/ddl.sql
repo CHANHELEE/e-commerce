9cb105aeab2cfa0658d619e579c32f4702ac8ed3
LEECHANHEE <chanhee1158@gmail>
2025-05-08 21:13:34 +0900

    <Feat> #30 : db ddl 수정

--- docs/db/ddl.sql
+++ docs/db/ddl.sql
@@ -1,169 +1,213 @@
--- 유저 테이블
-CREATE TABLE users
+create table coupons
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    name       VARCHAR(10) NOT NULL COMMENT '이름',
-    updated_at DATETIME(6) NOT NULL COMMENT '수정일시',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시'
+    id             bigint auto_increment comment '아이디'
+        primary key,
+    amount         int         not null comment '수량',
+    discount_price int         not null comment '할인 금액',
+    name           varchar(50) not null comment '이름',
+    created_at     datetime(6) not null comment '생성일시',
+    updated_at     datetime(6) not null comment '수정일시'
 );
 
--- 포인트 테이블
-CREATE TABLE points
+create table products
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    user_id    BIGINT      NOT NULL COMMENT '사용자 아이디',
-    point      INT         NOT NULL COMMENT '포인트',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at DATETIME(6) NOT NULL COMMENT '수정일시',
-    UNIQUE KEY (user_id),
-    CONSTRAINT fk_point_user FOREIGN KEY (user_id) REFERENCES users (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    name       varchar(50) not null comment '상품명',
+    price      int         not null comment '가격',
+    created_at datetime(6) not null comment '생성일시',
+    updated_at datetime(6) not null comment '수정일시'
 );
 
--- 포인트이력 테이블
-CREATE TABLE points_histories
+create table daily_popular_products
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    point_id   BIGINT      NOT NULL COMMENT '아이디',
-    point      INT         NOT NULL COMMENT '포인트',
-    type       VARCHAR(20) NOT NULL COMMENT '충전 타입',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    CONSTRAINT fk_point_history_point FOREIGN KEY (point_id) REFERENCES points (id)
+    id               bigint      not null comment '아이디'
+        primary key,
+    product_id       bigint      not null comment '상품아이디',
+    daily_sold_count int         not null comment '일일 판매 수량',
+    created_at       datetime(6) not null comment '생성일시',
+    constraint uk_daily_popular_product
+        unique (product_id),
+    constraint fk_daily_popular_product
+        foreign key (product_id) references products (id)
 );
 
--- 쿠폰 테이블
-CREATE TABLE coupons
+create table popular_product
 (
-    id             BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    amount         INT         NOT NULL COMMENT '수량',
-    discount_price INT         NOT NULL COMMENT '할인 금액',
-    name           VARCHAR(50) NOT NULL COMMENT '이름',
-    created_at     DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at     DATETIME(6) NOT NULL COMMENT '수정일시'
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    product_id bigint      not null comment '상품아이디',
+    name       varchar(50) not null comment '상품명',
+    ranking    int         not null comment '랭킹',
+    created_at datetime(6) not null comment '생성일시',
+    updated_at datetime(6) not null comment '수정일시',
+    constraint uk_popular_product
+        unique (product_id),
+    constraint fk_popular_product
+        foreign key (product_id) references products (id)
 );
 
--- 유저_쿠폰 테이블
-CREATE TABLE users_coupons
+create table products_options
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    coupon_id  BIGINT      NOT NULL COMMENT '쿠폰아이디',
-    user_id    BIGINT      NOT NULL COMMENT '사용자아이디',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at DATETIME(6) NOT NULL COMMENT '수정일시',
-    used_at    DATETIME(6) NULL COMMENT '사용일시',
-    UNIQUE KEY uk_user_coupon (user_id, coupon_id),
-    CONSTRAINT fk_user_coupon_coupon FOREIGN KEY (coupon_id) REFERENCES coupons (id),
-    CONSTRAINT fk_user_coupon_user FOREIGN KEY (user_id) REFERENCES users (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    product_id bigint      not null comment '상품아이디',
+    size       varchar(30) not null comment '규격',
+    stock      bigint      not null comment '재고',
+    created_at datetime(6) not null comment '생성일시',
+    updated_at datetime(6) not null comment '수정일시',
+    constraint uk_product_option
+        unique (product_id, size),
+    constraint fk_option_product
+        foreign key (product_id) references products (id)
 );
 
-
-CREATE TABLE products
+create table product_stock
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    name       VARCHAR(50) NOT NULL COMMENT '상품명',
-    price      INT         NOT NULL COMMENT '가격',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at DATETIME(6) NOT NULL COMMENT '수정일시'
+    id                bigint auto_increment comment '아이디'
+        primary key,
+    product_id        bigint      not null comment '상품아이디',
+    product_option_id bigint      not null comment '상품옵션아이디',
+    stock             bigint      not null comment '재고',
+    created_at        datetime(6) not null comment '생성일시',
+    updated_at        datetime(6) not null comment '수정일시',
+    constraint uk_product_stock
+        unique (product_id, product_option_id),
+    constraint fk_stock_product
+        foreign key (product_id) references products (id),
+    constraint fk_stock_product_option
+        foreign key (product_option_id) references products_options (id)
 );
 
-CREATE TABLE products_options
+create table users
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    product_id BIGINT      NOT NULL COMMENT '상품아이디',
-    size       VARCHAR(30) NOT NULL COMMENT '규격',
-    stock      BIGINT      NOT NULL COMMENT '재고',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at DATETIME(6) NOT NULL COMMENT '수정일시',
-    UNIQUE KEY uk_product_option (product_id, size),
-    CONSTRAINT fk_option_product FOREIGN KEY (product_id) REFERENCES products (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    name       varchar(10) not null comment '이름',
+    updated_at datetime(6) not null comment '수정일시',
+    created_at datetime(6) not null comment '생성일시'
 );
 
-
-CREATE TABLE product_stock
+create table points
 (
-    id                BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    product_id        BIGINT      NOT NULL COMMENT '상품아이디',
-    product_option_id BIGINT      NOT NULL COMMENT '상품옵션아이디',
-    stock             BIGINT      NOT NULL COMMENT '재고',
-    created_at        DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at        DATETIME(6) NOT NULL COMMENT '수정일시',
-    UNIQUE KEY uk_product_stock (product_id, product_option_id),
-    CONSTRAINT fk_stock_product FOREIGN KEY (product_id) REFERENCES products (id),
-    CONSTRAINT fk_stock_product_option FOREIGN KEY (product_option_id) REFERENCES products_options (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    user_id    bigint      not null comment '사용자 아이디',
+    point      int         not null comment '포인트',
+    created_at datetime(6) not null comment '생성일시',
+    updated_at datetime(6) not null comment '수정일시',
+    constraint user_id
+        unique (user_id),
+    constraint fk_point_user
+        foreign key (user_id) references users (id)
 );
 
-
-CREATE TABLE popular_product
+create table points_histories
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    product_id BIGINT      NOT NULL COMMENT '상품아이디',
-    name       VARCHAR(50) NOT NULL COMMENT '상품명',
-    rank       INT         NOT NULL COMMENT '랭킹',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    UNIQUE KEY uk_popular_product (product_id),
-    CONSTRAINT fk_popular_product FOREIGN KEY (product_id) REFERENCES products (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    point_id   bigint      not null comment '아이디',
+    point      int         not null comment '포인트',
+    type       varchar(20) not null comment '충전 타입',
+    created_at datetime(6) not null comment '생성일시',
+    constraint fk_point_history_point
+        foreign key (point_id) references points (id)
 );
 
-CREATE TABLE orders
+create table users_coupons
 (
-    id             BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    user_id        BIGINT      NOT NULL COMMENT '사용자아이디',
-    user_coupon_id BIGINT      NULL COMMENT '사용자_쿠폰_아이디',
-    status         VARCHAR(30) NOT NULL COMMENT '주문상태',
-    created_at     DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at     DATETIME(6) NOT NULL COMMENT '수정일시',
-    deleted_at     DATETIME(6) NULL COMMENT '삭제일시',
-    CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_order_user_coupon FOREIGN KEY (user_coupon_id) REFERENCES users_coupons (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    coupon_id  bigint      not null comment '쿠폰아이디',
+    user_id    bigint      not null comment '사용자아이디',
+    created_at datetime(6) not null comment '생성일시',
+    updated_at datetime(6) not null comment '수정일시',
+    used_at    datetime(6) null comment '사용일시',
+    constraint uk_user_coupon
+        unique (user_id, coupon_id),
+    constraint fk_user_coupon_coupon
+        foreign key (coupon_id) references coupons (id),
+    constraint fk_user_coupon_user
+        foreign key (user_id) references users (id)
 );
 
-CREATE TABLE orders_products
+create table orders
 (
-    id                BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    order_id          BIGINT      NOT NULL COMMENT '주문아이디',
-    product_id        BIGINT      NOT NULL COMMENT '상품아이디',
-    product_option_id BIGINT      NOT NULL COMMENT '상품옵션아이디',
-    product_price     INT         NOT NULL COMMENT '상품가격',
-    amount            INT         NOT NULL COMMENT '수량',
-    created_at        DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at        DATETIME(6) NOT NULL COMMENT '수정일시',
-    deleted_at        DATETIME(6) NULL COMMENT '삭제일시',
-    UNIQUE KEY uk_order_product (order_id, product_id, product_option_id),
-    CONSTRAINT fk_order_product_order FOREIGN KEY (order_id) REFERENCES orders (id),
-    CONSTRAINT fk_order_product_product FOREIGN KEY (product_id) REFERENCES products (id),
-    CONSTRAINT fk_order_product_option FOREIGN KEY (product_option_id) REFERENCES products_options (id)
+    id             bigint auto_increment comment '아이디'
+        primary key,
+    user_id        bigint      not null comment '사용자아이디',
+    user_coupon_id bigint      null comment '사용자_쿠폰_아이디',
+    status         varchar(30) not null comment '주문상태',
+    created_at     datetime(6) not null comment '생성일시',
+    updated_at     datetime(6) not null comment '수정일시',
+    deleted_at     datetime(6) null comment '삭제일시',
+    constraint fk_order_user
+        foreign key (user_id) references users (id),
+    constraint fk_order_user_coupon
+        foreign key (user_coupon_id) references users_coupons (id)
 );
 
-CREATE TABLE orders_histories
+create table orders_histories
 (
-    id         BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    order_id   BIGINT      NOT NULL COMMENT '주문아이디',
-    status     VARCHAR(30) NOT NULL COMMENT '주문상태',
-    created_at DATETIME(6) NOT NULL COMMENT '생성일시',
-    CONSTRAINT fk_order_history_order FOREIGN KEY (order_id) REFERENCES orders (id)
+    id         bigint auto_increment comment '아이디'
+        primary key,
+    order_id   bigint      not null comment '주문아이디',
+    status     varchar(30) not null comment '주문상태',
+    created_at datetime(6) not null comment '생성일시',
+    constraint fk_order_history_order
+        foreign key (order_id) references orders (id)
 );
 
-CREATE TABLE payment
+create table orders_products
 (
-    id                 BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    order_id           BIGINT      NOT NULL COMMENT '주문아이디',
-    status             VARCHAR(30) NOT NULL COMMENT '결제상태',
-    origin_total_price INT         NOT NULL COMMENT '실제총금액',
-    pay_total_price    INT         NOT NULL COMMENT '실결제금액',
-    discount_price     INT         NULL COMMENT '할인금액',
-    created_at         DATETIME(6) NOT NULL COMMENT '생성일시',
-    updated_at         DATETIME(6) NOT NULL COMMENT '수정일시',
-    UNIQUE KEY uk_payment_order (order_id),
-    CONSTRAINT fk_payment_order FOREIGN KEY (order_id) REFERENCES orders (id)
+    id                bigint auto_increment comment '아이디'
+        primary key,
+    order_id          bigint      not null comment '주문아이디',
+    product_id        bigint      not null comment '상품아이디',
+    product_option_id bigint      not null comment '상품옵션아이디',
+    product_price     int         not null comment '상품가격',
+    amount            int         not null comment '수량',
+    created_at        datetime(6) not null comment '생성일시',
+    updated_at        datetime(6) not null comment '수정일시',
+    deleted_at        datetime(6) null comment '삭제일시',
+    constraint uk_order_product
+        unique (order_id, product_id, product_option_id),
+    constraint fk_order_product_option
+        foreign key (product_option_id) references products_options (id),
+    constraint fk_order_product_order
+        foreign key (order_id) references orders (id),
+    constraint fk_order_product_product
+        foreign key (product_id) references products (id)
 );
 
-CREATE TABLE payment_histories
+create table payment
 (
-    id                 BIGINT      NOT NULL PRIMARY KEY COMMENT '아이디',
-    payment_id         BIGINT      NOT NULL COMMENT '결제아이디',
-    pay_total_price    INT         NOT NULL COMMENT '실제 결제금액',
-    discount_price     INT         NOT NULL COMMENT '할인 금액',
-    origin_total_price INT         NOT NULL COMMENT '실제 총금액',
-    status             VARCHAR(30) NOT NULL COMMENT '주문상태',
-    created_at         DATETIME(6) NOT NULL COMMENT '생성일시',
-    CONSTRAINT fk_payment_status_history_payment FOREIGN KEY (payment_id) REFERENCES payment (id)
+    id                 bigint auto_increment comment '아이디'
+        primary key,
+    order_id           bigint      not null comment '주문아이디',
+    status             varchar(30) not null comment '결제상태',
+    origin_total_price int         not null comment '실제총금액',
+    pay_total_price    int         not null comment '실결제금액',
+    discount_price     int         null comment '할인금액',
+    created_at         datetime(6) not null comment '생성일시',
+    updated_at         datetime(6) not null comment '수정일시',
+    constraint uk_payment_order
+        unique (order_id),
+    constraint fk_payment_order
+        foreign key (order_id) references orders (id)
 );
+
+create table payment_histories
+(
+    id                 bigint auto_increment comment '아이디'
+        primary key,
+    payment_id         bigint      not null comment '결제아이디',
+    pay_total_price    int         not null comment '실제 결제금액',
+    discount_price     int         not null comment '할인 금액',
+    origin_total_price int         not null comment '실제 총금액',
+    status             varchar(30) not null comment '주문상태',
+    created_at         datetime(6) not null comment '생성일시',
+    constraint fk_payment_status_history_payment
+        foreign key (payment_id) references payment (id)
+);
+
